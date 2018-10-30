d656fbe63c79ab75401e0c582afaf6cf8b38d838
Scott10040 <43757301+Scott10040@users.noreply.github.com>
2018-10-30 18:33:44 -0400

    Update script.js

--- Display Name HW/script.js
+++ Display Name HW/script.js
@@ -1,27 +1,9 @@
 function displayName() {
 	let fname = document.getElementById("fname").value;
 	let lname = document.getElementById("lname").value;
-	// .value extrcts the human interpretation of the object itself
-	// without .value displays the object itself("fname")
-	let name = document.getElementById("name").innerHTML = "Hello, my name is " + fname + " " +lname;
-
-
-
-
-
-	//console.log("Your first name is: " + fname + " and your last name is: " + lname);
+	let name = document.getElementById("name").innerHTML = "Hello, my name is " + fname + " " +lna
+	
 
 	console.log(fname + lname);
-	//think of document and console like talking to Alexa
-	//the . says i want to do something with the document
-
-	//the DOM is like HTML and Javascript talking to each other
-	//they each need all the references from each other
-
-	//functions always have () in HTML and Javascript
-
-
-
-
-
+	
 } 
